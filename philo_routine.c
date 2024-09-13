dbedfa3c3038ea4dd82028a8ee9e775d41d57dc9
et-learns-to-code <155518416+et-learns-to-code@users.noreply.github.com>
2024-09-13 11:54:26 +0800

    handled for one philo edge case.

--- philo_routine.c
+++ philo_routine.c
@@ -6,18 +6,25 @@
 /*   By: etien <etien@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/10 10:40:00 by etien             #+#    #+#             */
-/*   Updated: 2024/09/13 11:24:19 by etien            ###   ########.fr       */
+/*   Updated: 2024/09/13 11:53:54 by etien            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
+// The death monitor thread is created at the start and rejoined
+// once the philosopher has ended his routine.
 // The routine will follow the sequence of eat > sleep > think.
-// However, even-numbered philosophers will start out sleeping.
-// The staggered schedules will help to prevent a deadlock situation
-// when picking up the forks.
-// Once a philosopher has eaten all his meals, his thread will be
-// terminated by breaking out of the loop and ending his routine.
+// At the start, even-numbered philosophers will be slightly delayed
+// by calling ft_usleep for 10 milliseconds.
+// This will minimize contention for picking up the forks during
+// the first round of eating. From then on, the philosophers will
+// have staggered schedules which will reduce the chances for deadlock.
+// A special check will sleep and terminate the routine if there is only
+// one philosopher since he has only one fork and will be unable to eat.
+// Once a philosopher has had all his meals, he will end his routine.
+// The philosopher will only repeat his routine so long as
+// the shared dead_philo flag has not been marked as true.
 void	*philo_routine(void *arg)
 {
 	t_philo		*philo;
@@ -30,6 +37,11 @@ void	*philo_routine(void *arg)
 		ft_usleep(10);
 	while (!(any_philo_dead(philo)))
 	{
+		if (philo->data->nbr_philos == 1)
+		{
+			ft_usleep(philo->data->time_to_die);
+			break ;
+		}
 		philo_eats(philo);
 		if (philo->meals_eaten == philo->data->nbr_meals)
 		{
